c7ecdc977f4155db71fa0ab7a71616dc35736d16
Dmytro Petyshyn <dev6012@gmail.com>
2023-12-27 22:59:28 +0200

    add e2e tests and validation pipe for review

--- src/review/dto/create-review.dto.ts
+++ src/review/dto/create-review.dto.ts
@@ -1,7 +1,20 @@
+import { IsNumber, IsString, Max, Min } from 'class-validator';
+
 export class CreateReviewDto {
+	@IsString()
 	name: string;
+
+	@IsString()
 	title: string;
+
+	@IsString()
 	description: string;
-	raiting: number;
+
+	@Max(5, { message: 'Rating cant be more then 5!' })
+	@Min(1, { message: 'Rating cant be less then 1!' })
+	@IsNumber()
+	rating: number;
+
+	@IsString()
 	productId: string;
 }
